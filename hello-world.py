3242faf3e5d28c709677b31593d97006f1ecf3d2
Shubham Bajpai <shubham.bajpai@news.co.uk>
2020-01-30 13:03:50 +0530

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -3,7 +3,7 @@ class Demo:
     def print_greetings(self, name):
         print(f"hello {name}")
 
-        
+
 if __name__ == "__main__":
     demo_object = Demo()
     demo_object.print_greetings("haq")
